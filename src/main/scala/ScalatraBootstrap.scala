8d677ececc1d9d30facb3c499fa8e52df15099f5
narhen <henrik.narstad@knowit.no>
2017-06-26 13:52:36 +0200

    #447: boilerplate

---
+++ src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,18 @@
+/*
+ * Part of NDLA search_api.
+ * Copyright (C) 2017 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+import javax.servlet.ServletContext
+
+import org.scalatra.LifeCycle
+
+class ScalatraBootstrap extends LifeCycle {
+
+  override def init(context: ServletContext) {
+  }
+
+}
---
+++ src/main/scala/no/ndla/searchapi/SearchApiProperties.scala
@@ -0,0 +1,52 @@
+/*
+ * Part of NDLA search_api.
+ * Copyright (C) 2017 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+
+package no.ndla.searchapi
+
+import com.typesafe.scalalogging.LazyLogging
+import no.ndla.network.Domains
+import no.ndla.network.secrets.Secrets.readSecrets
+
+import scala.util.Properties._
+import scala.util.{Failure, Success}
+
+object SearchApiProperties extends LazyLogging {
+  val SecretsFile = "search-api.secrets"
+
+  val ApplicationPort = 80
+  val ContactEmail = "christergundersen@ndla.no"
+  val Environment = propOrElse("NDLA_ENVIRONMENT", "local")
+
+  val CorrelationIdKey = "correlationID"
+  val CorrelationIdHeader = "X-Correlation-ID"
+
+  lazy val Domain = Domains.get(Environment)
+
+  lazy val secrets = readSecrets(SecretsFile) match {
+     case Success(values) => values
+     case Failure(exception) => throw new RuntimeException(s"Unable to load remote secrets from $SecretsFile", exception)
+   }
+
+  def booleanProp(key: String) = prop(key).toBoolean
+
+  def prop(key: String): String = {
+    propOrElse(key, throw new RuntimeException(s"Unable to load property $key"))
+  }
+
+  def propOrElse(key: String, default: => String): String = {
+    secrets.get(key).flatten match {
+      case Some(secret) => secret
+      case None =>
+        envOrNone(key) match {
+          case Some(env) => env
+          case None => default
+        }
+    }
+  }
+}
