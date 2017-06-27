e2ac6f5f6bb01fb53f70d2a9e6f4a5407353c204
narhen <henrik.narstad@knowit.no>
2017-06-27 10:23:09 +0200

    #447: dont need secrets

--- src/main/scala/no/ndla/searchapi/SearchApiProperties.scala
+++ src/main/scala/no/ndla/searchapi/SearchApiProperties.scala
@@ -17,8 +17,6 @@ import scala.util.Properties._
 import scala.util.{Failure, Success}
 
 object SearchApiProperties extends LazyLogging {
-  val SecretsFile = "search-api.secrets"
-
   val ApplicationPort = 80
   val ContactEmail = "christergundersen@ndla.no"
   val Environment = propOrElse("NDLA_ENVIRONMENT", "local")
@@ -30,25 +28,11 @@ object SearchApiProperties extends LazyLogging {
 
   lazy val Domain = Domains.get(Environment)
 
-  lazy val secrets = readSecrets(SecretsFile) match {
-     case Success(values) => values
-     case Failure(exception) => throw new RuntimeException(s"Unable to load remote secrets from $SecretsFile", exception)
-   }
-
   def booleanProp(key: String) = prop(key).toBoolean
 
   def prop(key: String): String = {
     propOrElse(key, throw new RuntimeException(s"Unable to load property $key"))
   }
 
-  def propOrElse(key: String, default: => String): String = {
-    secrets.get(key).flatten match {
-      case Some(secret) => secret
-      case None =>
-        envOrNone(key) match {
-          case Some(env) => env
-          case None => default
-        }
-    }
-  }
+  def propOrElse(key: String, default: => String): String = envOrElse(key, default)
 }
