17ce4e995fcabd8e22731298249d019490768346
Jonas Natten <jonas.natten@knowit.no>
2019-11-14 14:29:27 +0100

    Test connecton to elasticsearch in healthcontroller

--- src/main/scala/no/ndla/searchapi/controller/HealthController.scala
+++ src/main/scala/no/ndla/searchapi/controller/HealthController.scala
@@ -8,15 +8,41 @@
 
 package no.ndla.searchapi.controller
 
-import org.scalatra.{Ok, ScalatraServlet}
+import com.sksamuel.elastic4s.http.ElasticDsl._
+import com.sksamuel.elastic4s.http.RequestSuccess
+import com.typesafe.scalalogging.LazyLogging
+import no.ndla.searchapi.integration.Elastic4sClient
+import org.scalatra.{InternalServerError, Ok, ScalatraServlet}
+
+import scala.util.{Failure, Success}
 
 trait HealthController {
+  this: Elastic4sClient =>
   val healthController: HealthController
 
-  class HealthController extends ScalatraServlet {
-
+  class HealthController extends ScalatraServlet with LazyLogging {
     get("/") {
-      Ok()
+      e4sClient.execute(clusterHealth()) match {
+        case Failure(exception) =>
+          logger.error("Something went wrong when contacting elasticsearch instance when performing health check",
+                       exception)
+          InternalServerError()
+        case Success(RequestSuccess(status, body, headers, result)) =>
+          if (status == 200) {
+            Ok()
+          } else {
+            logger.error(
+              s"""Health check against elasticsearch failed --->
+                 |Status: $status
+                 |Body: ${body.getOrElse("<missing>")}
+                 |headers:   ${headers.mkString("\n\t")}
+                 |Result: ${result.toString}
+                 |""".stripMargin
+            )
+            InternalServerError()
+          }
+      }
     }
+
   }
 }
