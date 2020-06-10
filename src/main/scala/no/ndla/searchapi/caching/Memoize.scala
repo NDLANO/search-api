5b442978df3d9c06f2680590d866be75a91468e0
Jonas Natten <jonas.natten@knowit.no>
2020-06-10 08:32:47 +0200

    #2153: Move sideeffect to onComplete rather than after awaiting future

--- src/main/scala/no/ndla/searchapi/caching/Memoize.scala
+++ src/main/scala/no/ndla/searchapi/caching/Memoize.scala
@@ -14,6 +14,7 @@ import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService, Future}
+import scala.util.{Failure, Success}
 
 class Memoize[R](maxCacheAgeMs: Long, f: () => R) extends (() => R) with LazyLogging {
   implicit val ec: ExecutionContextExecutorService =
@@ -27,29 +28,37 @@ class Memoize[R](maxCacheAgeMs: Long, f: () => R) extends (() => R) with LazyLog
   private[this] var cache: Option[CacheValue] = None
   private[this] var isUpdating: Option[Future[CacheValue]] = None
 
-  private def scheduleRenewCache: Future[CacheValue] = synchronized {
+  private def scheduleRenewCache(): Future[CacheValue] = synchronized {
     val fut = Future {
       CacheValue(f(), System.currentTimeMillis())
     }
 
     isUpdating = Some(fut)
+
+    fut.onComplete {
+      case Success(value) => updateCache(value)
+      case Failure(ex)    => logger.error(s"Failed to update memoized function. Failed with: ${ex.getMessage}", ex)
+    }
     fut
   }
 
-  def apply(): R = synchronized {
+  def apply(): R = {
     cache match {
       case Some(cachedValue) if !cachedValue.isExpired => cachedValue.value
       case _ =>
         val fut = isUpdating match {
           case Some(future) => future
-          case None         => scheduleRenewCache
+          case None         => scheduleRenewCache()
         }
 
-        val res = Await.result(fut, Duration.Inf)
-        isUpdating = None
-        cache = Some(res)
-        res.value
+        Await.result(fut, Duration.Inf).value
     }
+  }
+
+  def updateCache(result: CacheValue): Unit = {
+    isUpdating = None
+    cache = Some(result)
+    System.gc()
   }
 }
 
