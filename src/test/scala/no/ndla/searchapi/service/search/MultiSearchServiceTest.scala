d54b26576eeb1568acc579cee609d3c5ffd7a6ac
Gunnar Velle <gunnar.velle@gmail.com>
2020-09-14 08:44:56 +0200

    More explicit test

--- src/test/scala/no/ndla/searchapi/service/search/MultiSearchServiceTest.scala
+++ src/test/scala/no/ndla/searchapi/service/search/MultiSearchServiceTest.scala
@@ -373,6 +373,8 @@ class MultiSearchServiceTest extends IntegrationSuite with TestEnvironment {
       multiSearchService.matchingQuery(searchSettings.copy(subjects = List("urn:subject:2"), language = "all"))
     search.totalCount should be(7)
     search.results.head.contexts.length should be(2)
+    search.results.head.contexts
+      .map(_.subjectId) should be(List("urn:subject:1", "urn:subject:2")) // urn:subject:3 is not visible
     search.results.map(_.id) should be(Seq(1, 5, 5, 6, 7, 11, 12))
   }
 
