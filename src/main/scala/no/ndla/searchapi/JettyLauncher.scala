ead876c5412d5ca7ac11c07879d3b5b260935f81
Jonas Natten <jonas.natten@knowit.no>
2021-04-20 10:08:18 +0200

    Move standalone indexing to separate file and add slack message sending

--- src/main/scala/no/ndla/searchapi/JettyLauncher.scala
+++ src/main/scala/no/ndla/searchapi/JettyLauncher.scala
@@ -10,19 +10,15 @@ package no.ndla.searchapi
 
 import com.typesafe.scalalogging.LazyLogging
 import net.bull.javamelody.{MonitoringFilter, Parameter, ReportServlet, SessionListener}
-import no.ndla.searchapi.model.domain.{Content, ReindexResult}
+import no.ndla.searchapi.service.StandaloneIndexing
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, FilterHolder, ServletContextHandler}
 import org.scalatra.servlet.ScalatraListener
 
 import java.util
-import java.util.concurrent.Executors
 import javax.servlet.DispatcherType
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService, Future}
 import scala.io.Source
 import scala.jdk.CollectionConverters.MapHasAsScala
-import scala.util.{Failure, Success, Try}
 
 object JettyLauncher extends LazyLogging {
 
@@ -33,7 +29,7 @@ object JettyLauncher extends LazyLogging {
     envMap.asScala.foreach { case (k, v) => System.setProperty(k, v) }
 
     if (SearchApiProperties.booleanOrFalse("STANDALONE_INDEXING_ENABLED")) {
-      doStandaloneIndexing()
+      StandaloneIndexing.doStandaloneIndexing()
     } else {
       val server = startServer(SearchApiProperties.ApplicationPort)
       server.join()
@@ -69,56 +65,6 @@ object JettyLauncher extends LazyLogging {
     logger.info(s"Started at port $port in $startTime ms.")
 
     server
-  }
-
-  private def doStandaloneIndexing() = {
-    val bundles = for {
-      taxonomyBundle <- ComponentRegistry.taxonomyApiClient.getTaxonomyBundle()
-      grepBundle <- ComponentRegistry.grepApiClient.getGrepBundle()
-    } yield (taxonomyBundle, grepBundle)
-
-    val start = System.currentTimeMillis()
-
-    bundles match {
-      case Failure(ex) => throw ex
-      case Success((taxonomyBundle, grepBundle)) =>
-        implicit val ec: ExecutionContextExecutorService =
-          ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(SearchApiProperties.SearchIndexes.size))
-
-        def reindexWithIndexService[C <: Content](indexService: ComponentRegistry.IndexService[C])(
-            implicit mf: Manifest[C]): Future[Try[ReindexResult]] = {
-          val reindexFuture = Future { indexService.indexDocuments(taxonomyBundle, grepBundle) }
-          reindexFuture.onComplete {
-            case Success(Success(reindexResult: ReindexResult)) =>
-              logger.info(
-                s"Completed indexing of ${reindexResult.totalIndexed} ${indexService.searchIndex} in ${reindexResult.millisUsed} ms.")
-            case Success(Failure(ex)) => logger.warn(ex.getMessage, ex)
-            case Failure(ex) =>
-              logger.warn(s"Unable to create index '${indexService.searchIndex}': " + ex.getMessage, ex)
-          }
-
-          reindexFuture
-        }
-
-        val result = Await.result(
-          Future.sequence(
-            Seq(
-              reindexWithIndexService(ComponentRegistry.learningPathIndexService),
-              reindexWithIndexService(ComponentRegistry.articleIndexService),
-              reindexWithIndexService(ComponentRegistry.draftIndexService)
-            )),
-          Duration.Inf
-        )
-
-        result.foreach(x =>
-          if (x.isFailure) {
-            logger.error("Indexing failed...")
-            sys.exit(1)
-        })
-
-        logger.info(s"Reindexing all indexes took ${System.currentTimeMillis() - start} ms...")
-        sys.exit(0)
-    }
   }
 
 }
